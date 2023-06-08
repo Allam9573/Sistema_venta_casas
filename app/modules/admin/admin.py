563fb9df2b1cf0b6f56617ba2dc70c4cc9c278b8
Allam9573 <aallamdaniel@gmail.com>
2023-06-08 12:05:35 -0600

    busqueda por nombre de propietario

--- app/modules/admin/admin.py
+++ app/modules/admin/admin.py
@@ -1,14 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from app.modules.db.db import get_connection
 
 bp = Blueprint('admin', __name__, url_prefix='/admin')
 
+con = get_connection()
 
 @bp.route('/houses_list')
 def houses_list():
-    con = get_connection()
     cursor = con.cursor(buffered=True)
     cursor.execute('SELECT * FROM casas')
     con.commit()
     data = cursor.fetchall()
+    return render_template('house/admin_list.html', data=data)
+
+
+@bp.route('/houses_list_id', methods=['POST','GET'])
+def get_house_id():
+    data = ''
+    if request.method == 'POST':
+        cursor = con.cursor(buffered=True)
+        vendedor = request.form['buscar']
+        cursor.execute('SELECT * FROM casas WHERE propietario=%s', [vendedor])
+        data=cursor.fetchall()
     return render_template('house/admin_list.html', data=data)
